fba1dd6b78695a611c68ec0abb95b3a5be6ae23d
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-03 10:42:14 -0600

    feat: Se añadio la ruta de ContainerWidget

--- basic_components/lib/navigator/controller.dart
+++ basic_components/lib/navigator/controller.dart
@@ -1,11 +1,13 @@
 import 'package:basic_components/pages/main_page.dart';
+import 'package:basic_components/widgets/ContainerWidget.dart';
 
 import 'package:flutter/widgets.dart';
 
 class Controller {
   static getRoutes(BuildContext context) {
     return {
-      '/MainPage': (context) => MainPage(),
+      '/MainPage': (context) => const MainPage(),
+      '/ContainerWidget': (context) => const ContainerWidget(),
     };
   }
 }
