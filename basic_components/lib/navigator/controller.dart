13b963a06e9b5fd9b34cdd512e42a46042f2c674
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-03 11:00:45 -0600

    feat: Se añadio la ruta de AppBarWidget

--- basic_components/lib/navigator/controller.dart
+++ basic_components/lib/navigator/controller.dart
@@ -1,5 +1,7 @@
 import 'package:basic_components/pages/main_page.dart';
+import 'package:basic_components/widgets/AppBarWidget.dart';
 import 'package:basic_components/widgets/ContainerWidget.dart';
+import 'package:basic_components/widgets/TextWidget.dart';
 
 import 'package:flutter/widgets.dart';
 
@@ -8,6 +10,9 @@ class Controller {
     return {
       '/MainPage': (context) => const MainPage(),
       '/ContainerWidget': (context) => const ContainerWidget(),
+      '/TextWidget': (context) => const TextWidget(),
+      '/AppBarWidget': (context) => const AppBarWidget(),
+
     };
   }
 }
