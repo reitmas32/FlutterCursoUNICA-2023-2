8052355a2dd2474fd393f1d76d44dddb8aeca363
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-19 06:47:24 -0600

    feat: Creacion del Ejemplo Text

---
+++ WidgetsBasicos/text_widget/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World!!! Im in a Course UNICA',
+          style: TextStyle(fontSize: 25.0),
+        ),
+      ),
+    );
+  }
+}
