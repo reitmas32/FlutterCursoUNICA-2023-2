b852a61559ce17f6be9b021f9bd3794a4e8e7d0c
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-19 06:43:21 -0600

    feat: Creacion del Ejemplo Container

---
+++ WidgetsBasicos/container_widget/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.amber,
+          height: 100,
+          width: 100,
+        ),
+      ),
+    );
+  }
+}
