f521c88b5f40b9436876cbbfd5ec9f6ffe2a9f37
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-19 06:59:20 -0600

    feat: Creacion del Ejemplo Row

---
+++ WidgetsBasicos/row_widget/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: const [
+            Text(
+              'Sample Code',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25.0,),
+            ),
+            Text(
+              'Hello Wolrd!!!',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25.0,),
+            ),
+            FlutterLogo(size: 200.0),
+          ],
+        ),
+      ),
+    );
+  }
+}
