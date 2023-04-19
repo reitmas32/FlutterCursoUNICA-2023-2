23a932ea2e7e4bdf40d41fbe46793c50f498425d
reitmas32 <rafa.zamora.rals@gmail.com>
2023-04-19 07:26:35 -0600

    feat: Dart Tipos Primitivos

---
+++ DartEjemplos/tipos_primitivos.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Numerics
+  int entero = 5;
+  double flotante = 5.0;
+  
+  // booleans
+  bool booleano = false;
+  
+  // Strings
+  String cadena_commillas_dobles = "Hello World!!!";
+  String cadena_commillas_simple = 'Hello World!!!';
+  
+  // Dynamic
+  dynamic variable_adaptable = 3;
+  variable_adaptable = "Nueva Cadena";
+  variable_adaptable = 5.9;
+  
+  // Tipo Deducible
+  var cadena_con_tipo_deducible = "Hola Mundo!!!";
+}
